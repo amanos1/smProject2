f4f269921281413adc6b3e4f6268a9232327735a
harshPatel1818 <91505652+harshPatel1818@users.noreply.github.com>
2022-02-28 20:48:22 -0500

    Update MoneyMarketTest.java

--- src/smProject2/MoneyMarketTest.java
+++ src/smProject2/MoneyMarketTest.java
@@ -5,8 +5,18 @@ import static org.junit.jupiter.api.Assertions.*;
 public class MoneyMarketTest
 {
 	@org.junit.Test
-	void test() {
-		fail("Not yet implemented");
+	public void monthlyInterest() 
+	{
+		MoneyMarket mm;
+		Profile h = new Profile("Harsh", "Patel","8/18/2001");
+		mm = new MoneyMarket(h,3000);
+		//Loyal account with 0.95% Annual Interest Rate
+		assertTrue(mm.monthlyInterest() == 2.38);
+		
+		//withdrawing 1000 will make the balance drop below 2500 which will make the accout NOT loyal
+		mm.withdraw(1000);
+		//Calculated monthly interest using 0.8% as annual interest rate
+		assertTrue(mm.monthlyInterest() == 1.33);
 	}
 
 }
